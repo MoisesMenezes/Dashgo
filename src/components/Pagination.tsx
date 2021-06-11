6ec538ba3d40dede7fa9f01a2ca5999d5b0b388d
Moises Menezes <moisesmenezes90@gmail.com>
2021-06-10 22:34:24 -0300

    feat: add create user page

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -13,41 +13,41 @@ export function Pagination() {
         <strong>0</strong> - <strong>10</strong> de <strong>100</strong>
       </Box>
       <Stack direction="row" spacing="2">
-      <Button
-        size="sm"
-        fontSize="xs"
-        width="4"
-        colorScheme="pink"
-        disabled
-        _disabled={{
-          bgColor: "pink.500",
-          cursor: "default",
-        }}
-      >
-        1
-      </Button>
-      <Button
-        size="sm"
-        fontSize="xs"
-        width="4"
-        bgColor="gray.700"
-        _hover={{
-          bg: "gray.500",
-        }}
-      >
-        2
-      </Button>
-      <Button
-        size="sm"
-        fontSize="xs"
-        width="4"
-        bgColor="gray.700"
-        _hover={{
-          bg: "gray.500",
-        }}
-      >
-        3
-      </Button>
+        <Button
+          size="sm"
+          fontSize="xs"
+          width="4"
+          colorScheme="pink"
+          disabled
+          _disabled={{
+            bgColor: "pink.500",
+            cursor: "default",
+          }}
+        >
+          1
+        </Button>
+        <Button
+          size="sm"
+          fontSize="xs"
+          width="4"
+          bgColor="gray.700"
+          _hover={{
+            bg: "gray.500",
+          }}
+        >
+          2
+        </Button>
+        <Button
+          size="sm"
+          fontSize="xs"
+          width="4"
+          bgColor="gray.700"
+          _hover={{
+            bg: "gray.500",
+          }}
+        >
+          3
+        </Button>
       </Stack>
     </Stack>
   );
